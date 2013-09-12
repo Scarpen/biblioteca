9213cda11bcf7aaee4dd0a87f3dfe3a40c11d57c
felipegithub <felipefs92@gmail.com>
2013-09-12 11:39:57 -0300

    Mudei o Titulo XP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130903135642) do
+ActiveRecord::Schema.define(:version => 20130904125950) do
 
   create_table "endbiblis", :force => true do |t|
     t.string   "rua"
@@ -30,5 +30,10 @@ ActiveRecord::Schema.define(:version => 20130903135642) do
   end
 
   add_index "listbiblis", ["endbibli_id"], :name => "index_listbiblis_on_endbibli_id"
+
+  create_table "testes", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
