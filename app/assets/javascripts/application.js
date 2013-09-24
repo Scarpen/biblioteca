dcb7ff9597991e310dce36f2a65cefd948e54e32
Thiago <thiago@fabsoft-1.(none)>
2013-09-24 09:09:09 -0300

    testes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 function remove_fields (link){
+	alert("assadsdasdasdassda");
 	$(link).previous("input[type=hidden]").value="1";
-	$(link).up(".fields").hide();	
-
+	//$(link).up(".fields").hide();
+	alert("assadsdasdas");
 }
